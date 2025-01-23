5f8c857fa81a869241c265d78dce4e51f9e4722b
Zidna Fadla <yuratamma@gmail.com>
2025-01-23 16:22:23 +0700

    fix(settings): add default value for theme color picker

--- src/Pages/GeneralSettingsPage.php
+++ src/Pages/GeneralSettingsPage.php
@@ -124,48 +124,7 @@ class GeneralSettingsPage extends Page
             $arrTabs[] = Tabs\Tab::make('Application Tab')
                 ->label(__('filament-general-settings::default.application'))
                 ->icon('heroicon-o-tv')
-                ->schema([
-                    TextInput::make('site_name')
-                        ->label(__('filament-general-settings::default.site_name'))
-                        ->autofocus()
-                        ->columnSpanFull(),
-                    Textarea::make('site_description')
-                        ->label(__('filament-general-settings::default.site_description'))
-                        ->columnSpanFull(),
-                    Grid::make()->schema([
-                        FileUpload::make('site_logo')
-                            ->label(fn () => __('filament-general-settings::default.site_logo'))
-                            ->image()
-                            ->directory('assets')
-                            ->visibility('public')
-                            ->moveFiles()
-                            ->imageEditor()
-                            ->getUploadedFileNameForStorageUsing(fn () => 'site_logo.png')
-                            ->columnSpan(2),
-                        FileUpload::make('site_favicon')
-                            ->label(fn () => __('filament-general-settings::default.site_favicon'))
-                            ->image()
-                            ->directory('assets')
-                            ->visibility('public')
-                            ->moveFiles()
-                            ->getUploadedFileNameForStorageUsing(fn () => 'site_favicon.ico')
-                            ->acceptedFileTypes(['image/x-icon', 'image/vnd.microsoft.icon'])
-                            ->columnSpan(2),
-                    ])
-                        ->columns(4)
-                        ->visible(fn () => config('filament-general-settings.show_logo_and_favicon')),
-                    TextInput::make('support_email')
-                        ->label(__('filament-general-settings::default.support_email'))
-                        ->prefixIcon('heroicon-o-envelope'),
-                    TextInput::make('support_phone')
-                        ->prefixIcon('heroicon-o-phone')
-                        ->label(__('filament-general-settings::default.support_phone')),
-                    ColorPicker::make('theme_color')
-                        ->label(__('filament-general-settings::default.theme_color'))
-                        ->prefixIcon('heroicon-o-swatch')
-                        ->formatStateUsing(fn (?string $state): string => $state ?? config('filament.theme.colors.primary'))
-                        ->helperText(__('filament-general-settings::default.theme_color_helper_text')),
-                ])
+                ->schema(ApplicationFieldsForm::get())
                 ->columns(3);
         }
 
